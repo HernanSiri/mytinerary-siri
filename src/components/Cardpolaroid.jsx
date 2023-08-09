4e0bcda18a75c782a287075af48735e09510784a
hernan siris <masmusic_madryn@hotmail.comm>
2023-08-09 19:17:37 -0300

    creacion carousel

--- src/components/Cardpolaroid.jsx
+++ src/components/Cardpolaroid.jsx
@@ -2,9 +2,9 @@
 
 export default function Cardpolaroid({src,alt,text}) {
   return (
-    <div className='max-w-xs transition duration-300 ease-in-out hover:scale-110 flex flex-col items-center p-4 m-2 bg-emerald-50 rounded-lg shadow-lg cursor'>
+    <div className='max-w-xs transition duration-300 ease-in-out hover:scale-110 flex flex-col items-center p-4 m-2 bg-emerald-50 rounded-lg shadow-lg cursor '>
   <img
-    className='h-40 w-40 rounded-full object-cover object-center'
+    className='h-40 w-40 rounded-full object-cover object-center '
     src={src}
     alt={alt}
   />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,21 @@
+import { useState,useEffect } from "react"
 import Arrow from "../components/Arrow"
-import CardPolaroid from "../components/CardPolaroid"
+import axios from "axios"
+import Carousel from "../components/Carousel"
 
 export default function Home() {
-
-    let data = [
-        { id: 'america1', city: "Cancun", photo: "/img/america/cancun.jpg" },
-        { id: 'america2', city: "New York", photo: "/img/america/newyork.jpg" },
-        { id: 'america3', city: "Rio de Janeiro", photo: "/img/america/rioDeJaneiro.jpg" },
-        { id: 'america4', city: "Ushuaia", photo: "/img/america/ushuaia.jpg" },
-        { id: 'asia1', city: "Bangkok", photo: "/img/asia/bangkok.jpg" },
-        { id: 'asia2', city: "Pekin", photo: "/img/asia/pekin.jpg" },
-        { id: 'asia3', city: "Singapur", photo: "/img/asia/singapur.jpg" },
-        { id: 'asia4', city: "Tokyo", photo: "/img/asia/tokio.jpg" },
-        { id: 'europe1', city: "Ibiza", photo: "/img/europe/ibiza.jpg" },
-        { id: 'europe2', city: "London", photo: "/img/europe/london.jpg" },
-        { id: 'europe3', city: "Paris", photo: "/img/europe/paris.jpg" },
-        { id: 'europe4', city: "Roma", photo: "/img/europe/roma.jpg" },
-        { id: 'oceania1', city: "Majuro", photo: "/img/oceania/majuro.jpg" },
-        { id: 'oceania2', city: "Sidney", photo: "/img/oceania/sidney.jpg" },
-        { id: 'oceania3', city: "Suva", photo: "/img/oceania/suva.jpg" },
-        { id: 'oceania4', city: "Wellington", photo: "/img/oceania/wellington.jpg" }
-      ]
-
+    
+    const [show,setShow] = useState(true)
+    const [data,setData] = useState([])
+   
+      useEffect(
+        ()=>{
+          axios('/data.json')
+           .then(res=>setData(res.data))
+           .catch(err=>console.log(err))
+        },
+        []
+        )
     return (
         <main className='ml-10 mr-10 md:pt-44 mx-auto flex flex-col md:flex-row drop-shadow-lg'>
             <div className='md:w-1/2'>
@@ -33,13 +27,7 @@ export default function Home() {
                  View More
                 </a>
             </div>
-            <div className="xl:w-1/2 flex justify-center items-center">
-                <Arrow direction="M15.75 19.5L8.25 12l7.5-7.5" />
-                <div className="flex flex-grawp  w-11/12 justify-center">
-                    {data.slice(0, 4).map(each => <CardPolaroid key={each.id} src={each.photo} alt={each.id} text={each.city} />)}
-                </div>
-                <Arrow direction="M8.25 4.5l7.5 7.5-7.5 7.5" />
-            </div>
+            <Carousel data={data} />
         </main>
     )
 }
