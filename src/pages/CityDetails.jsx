35a7485fa880506cc347e7c7196092214ab00a07
hernan siris <masmusic_madryn@hotmail.comm>
2023-08-22 12:37:43 -0300

    city varios

--- src/pages/CityDetails.jsx
+++ src/pages/CityDetails.jsx
@@ -5,6 +5,17 @@ import { useParams } from 'react-router-dom';
 export default function CityDetails() {
   const { city_id } = useParams();
 
+  // export default function Cities() {
+  //   const [cities, setCities] = useState([]);
+  //   const [reEffect, setReEffect] = useState(true);
+  //   const text = useRef();
+  
+  //   useEffect(() => {
+  //     axios(apiUrl + 'cities?city=' + text.current.value)
+  //       .then((res) => setCities(res.data.response))
+  //       .catch((err) => console.log(err));
+  //   }, [reEffect]);
+
   return (
     <div className='rounded-full bg-white/50 bg-cover grow'>
       <h1 className='animate-pulse text-black text-[40px] flex justify-center items-center'>
