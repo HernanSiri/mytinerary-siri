66ee5c6144e596cdbf15f71d801f714b5ba8c90f
hernan siris <masmusic_madryn@hotmail.comm>
2023-08-23 00:48:01 -0300

    CityDetails en construccion

---
+++ src/components/CardError.jsx
@@ -0,0 +1,7 @@
+export default function CardError() {
+    return (
+        <div className='flex justify-center items-center rounded-full bg-white/50 bg-cover grow'>
+            <h1 className='animate-pulse text-black text-[40px] flex justify-center items-center'>sorry, please refine your search</h1>
+        </div>
+    )
+}
--- src/pages/CityDetails.jsx
+++ src/pages/CityDetails.jsx
@@ -1,33 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
+import apiUrl from '../../apiUrl';
 
 export default function CityDetails() {
-  const { city_id } = useParams();
+  const { _id } = useParams();
+  const [city, setCity] = useState([]);
 
-  // export default function Cities() {
-  //   const [cities, setCities] = useState([]);
-  //   const [reEffect, setReEffect] = useState(true);
-  //   const text = useRef();
-  
-  //   useEffect(() => {
-  //     axios(apiUrl + 'cities?city=' + text.current.value)
-  //       .then((res) => setCities(res.data.response))
-  //       .catch((err) => console.log(err));
-  //   }, [reEffect]);
+  useEffect(() => {
+    axios(apiUrl + 'cities/city/:'+ _id)
+      .then((res) =>{ setCity(res.data.response)
+        console.log(city)})
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className='rounded-full bg-white/50 bg-cover grow'>
       <h1 className='animate-pulse text-black text-[40px] flex justify-center items-center'>
         City under construction
       </h1>
-      {/* Botón para volver a la página de Cities */}
       <div className='flex justify-center mt-4'>
         <Link to="/cities" className='px-4 py-2 bg-emerald-300 rounded-lg text-white'>
           Back to Cities
         </Link>
       </div>
-      <p>City ID: {city_id}</p>
+      
+      <div className='text-black mt-10 flex justify-center items-center tex-xl'>
+        <h1>City Details {city.tex}</h1>
+      </div>
+      <div>
+          <img className='rounded-md' src="{city.photo}" alt="{city.country}" />
+      </div>
+      <div>
+        <p className='tex-black mt-8 ml-5'>{city.featuredlocation}</p>
+        <p className='tex-black mt-8 ml-5'>{city.description}</p>
+        <p className='tex-black mt-8 ml-5'>{city.smalldescription}</p>
+      </div>
     </div>
   );
 }
+
